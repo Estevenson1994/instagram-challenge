2441737adeea209354b2d02de4fe140ea6508a94
Erin Stevenson <estevenson_94@live.com>
2019-03-02 19:35:23 +0000

    :wrench: user id cannot be null in posts

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_03_02_172952) do
     t.text "caption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
